2e7e1b74a513439f4f5e10449f5356564ff5f331
Your Name <you@example.com>
2025-09-17 03:18:41 +0500

    completed

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -36,3 +36,63 @@ export const updateExpense = async (req, res) => {
     });
   }
 };
+
+export const deleteController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const existingExpense = await Expense.findById(id);
+    if (!existingExpense) {
+      return res.status(404).json({
+        message: "Expense does not exist",
+      });
+    }
+    await Expense.findByIdAndDelete(id);
+    return res.status(204).json({
+      message: "Expense deleted successfully.",
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Internal Server error",
+    });
+  }
+};
+
+export const getController = async (req, res) => {
+  try {
+    const { filter } = req.query;
+    if (filter === "month") {
+      const monthlyExpenses = await Expense.find({ month });
+      return res.status(200).json({
+        expenses: monthlyExpenses,
+      });
+    } else if (filter == "3months") {
+      const date = new Date();
+      const currMonth = date.getMonth();
+      const currYear = date.getFullYear();
+
+      function padMonth(m) {
+        return m < 10 ? `0${m}` : m;
+      }
+      if (currMonth - 3 >= 0 && currMonth <= 11) {
+        const m1 = `${currYear}-${padMonth(currMonth + 1)}`;
+        const m2 = `${currYear}-${padMonth(currMonth)}`;
+        const m3 = `${currYear}-${padMonth(currMonth - 1)}`;
+        console.log(m1, m2, m3);
+      }
+      const exp = await Expense.find({ month: { $in: [m1, m2, m3] } });
+      return res.status(200).json({
+        expenses: exp,
+      });
+    } else if (filter == "range") {
+      const { start, end } = req.query;
+      const exp = await Expense.find({ month: { $gte: start, $lte: end } });
+      return res.status(200).json({
+        expenses: exp,
+      });
+    }
+  } catch (e) {
+    return res.status(500).json({
+      message: "Internal Server error",
+    });
+  }
+};
--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import authenticationMiddleware from "../middleware/authentication.js";
-import { addExpense, updateExpense } from "../controllers/expenseController.js";
+import {
+  addExpense,
+  deleteController,
+  getController,
+  updateExpense,
+} from "../controllers/expenseController.js";
 import { authorizeMiddleware } from "../middleware/authorization.js";
 
 const router = express.Router();
@@ -8,6 +13,8 @@ const router = express.Router();
 router.route("/").post(authenticationMiddleware, addExpense);
 router
   .route("/:id")
-  .put(authenticationMiddleware, authorizeMiddleware, updateExpense);
+  .put(authenticationMiddleware, authorizeMiddleware, updateExpense)
+  .delete(authenticationMiddleware, authorizeMiddleware, deleteController);
 
+router.get("/", getController);
 export default router;
